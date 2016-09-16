d364e2e3541cd82f3b6cd75a5a248691cc935768
Noa <noamergi@gmail.com>
2016-09-16 00:28:02 -0400

    broat all planets closer because of jittering

--- SolarSystem/Assets/Script/CameraScript.cs
+++ SolarSystem/Assets/Script/CameraScript.cs
@@ -16,7 +16,7 @@ public class CameraScript : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-       transform.rotation = orientation;
+       transform.localRotation = orientation;
         //transform.Rotate(offset,Space.World);
 	}
 }
